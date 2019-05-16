0f8639bd3bedaecb4e447feffb5c247c98dea1db
hanpanpan200 <hanpanpan200@gmail.com>
2019-05-16 14:50:11 +0800

    feat: add autolayout constraints using storyboard

---
+++ AutoLayoutDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutDemo
+//
+//  Created by Panpan Han  on 2019/5/15.
+//  Copyright © 2019 PanpanHan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
